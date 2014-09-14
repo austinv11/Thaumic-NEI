a438fe74072248801ea24e4900f4531b8df0eb14
austinv11 <austinv11@gmail.com>
2014-09-14 19:45:54 -0400

    Moar aspect search options!
    You can now use @aspects instead of @aspect to search for certain
    aspects (syntax: @aspects:<aspect>,<another aspect>,<etc.>)

--- com/austinv11/thaumicnei/AspectFilter.java
+++ com/austinv11/thaumicnei/AspectFilter.java
@@ -8,6 +8,8 @@ import net.minecraft.item.ItemStack;
 import thaumcraft.api.aspects.Aspect;
 import thaumcraft.api.aspects.AspectList;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Pattern;
 import java.util.regex.PatternSyntaxException;
 
@@ -20,16 +22,19 @@ public class AspectFilter implements SearchField.ISearchProvider {
 
 	public static class Filter implements ItemFilter {
 		String pattern;
-		boolean aspectSearch = false;
+		int aspectSearch = 0;
 
 		public Filter(String searchText) {
 			//Logger.info("Searching! :D");
-			if (searchText.startsWith("@aspect:") && searchText.length() > 8){
+			if (searchText.startsWith("@aspect:") && searchText.length() > 8) {
 				pattern = searchText.substring(8);
-				aspectSearch = true;
+				aspectSearch = 1;
+			}else if (searchText.startsWith("@aspects:") && searchText.length() > 9){
+				pattern = searchText.substring(9);
+				aspectSearch = 2;
 			}else{
 				pattern = searchText;
-				aspectSearch = true;
+				aspectSearch = 0;
 			}
 		}
 
@@ -38,17 +43,49 @@ public class AspectFilter implements SearchField.ISearchProvider {
 			if (pattern == null || pattern == ""){
 				return true;
 			}else{
-				if (aspectSearch){
+				if (aspectSearch == 1) {//@aspect:
 					AspectList list = new AspectList(itemStack);
-					if (list != null){
+					if (list != null) {
 						for (Aspect aspect : list.getAspects()) {
-							if (aspect != null){
+							if (aspect != null) {
 								if (pattern.equalsIgnoreCase(aspect.getName())) {
 									return true;
 								}
 							}
 						}
 					}
+				}else if (aspectSearch == 2) {//@aspectS:
+					AspectList list = new AspectList(itemStack);
+					if (list != null) {
+						pattern = pattern.replace(" ","");
+						if (pattern.contains(",")){
+							String[] patterns = pattern.split(",");
+							if (patterns.length > 1) {
+								List<Boolean> match = new ArrayList<Boolean>();
+								for (Aspect aspect : list.getAspects()) {
+									for (String p : patterns) {
+										if (aspect != null){
+											if (p.equalsIgnoreCase(aspect.getName())) {
+												match.add(true);
+											}
+										}
+									}
+								}
+								if (match.size() == patterns.length) {
+									return true;
+								}else {
+									return false;
+								}
+							}
+						}else{
+							aspectSearch = 1;
+							pattern = pattern.replace(",","");
+							return this.matches(itemStack);
+						}
+					}else{
+						aspectSearch = 1;
+						return this.matches(itemStack);
+						}
 				}else{
 					return true;
 				}
