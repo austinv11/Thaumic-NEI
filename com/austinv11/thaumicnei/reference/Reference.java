8f0373495670746ca6915ebfd6418484f9ca6512
austinv11 <austinv11@gmail.com>
2014-09-14 19:16:06 -0400

    Revert 5f01653..be7567d
    This rolls back to commit 5f01653ba771d70546fafd1a74eeb5af9e629a7c.

--- com/austinv11/thaumicnei/reference/Reference.java
+++ com/austinv11/thaumicnei/reference/Reference.java
@@ -1,7 +1,7 @@
 package com.austinv11.thaumicnei.reference;
 
 public class Reference {
-	public static final String MOD_ID = "thaumicnei";
+	public static final String MOD_ID = "ThaumicNEI";
 	public static final String MOD_NAME = "Thaumic NEI";
 	public static final String VERSION = "@VERSION@";
 	public static final String SERVER_PROXY_CLASS = "com.austinv11.thaumicnei.proxy.ServerProxy";
