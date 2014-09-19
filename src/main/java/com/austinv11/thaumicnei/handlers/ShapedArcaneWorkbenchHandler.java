9a677e8f84e4ea3d075983752b8ff36f8f159106
austinv11 <austinv11@gmail.com>
2014-09-18 21:16:07 -0400

    Finally finished the ShapedArcaneRecipeHandler

--- src/main/java/com/austinv11/thaumicnei/handlers/ShapedArcaneWorkbenchHandler.java
+++ src/main/java/com/austinv11/thaumicnei/handlers/ShapedArcaneWorkbenchHandler.java
@@ -5,7 +5,6 @@ import codechicken.nei.PositionedStack;
 import codechicken.nei.recipe.TemplateRecipeHandler;
 import com.austinv11.thaumicnei.reference.Reference;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.StatCollector;
 import org.lwjgl.opengl.GL11;
 import thaumcraft.api.ThaumcraftApi;
@@ -14,21 +13,20 @@ import thaumcraft.api.aspects.Aspect;
 import thaumcraft.api.aspects.AspectList;
 import thaumcraft.api.crafting.ShapedArcaneRecipe;
 
+import java.awt.*;
 import java.util.ArrayList;
 import java.util.List;
-
-import static thaumcraft.api.aspects.Aspect.*;
 
 public class ShapedArcaneWorkbenchHandler extends TemplateRecipeHandler {
 
 	@Override
 	public String getGuiTexture() {
-		return "thaumicnei:textures/gui/gui_arcaneworkbench.png";
+		return "thaumcraft:textures/gui/gui_arcaneworkbench.png";
 	}
 
 	@Override
 	public String getRecipeName() {
-		return StatCollector.translateToLocal("thaumicnei:gui.nei.arcaneWorkbench.shaped");
+		return StatCollector.translateToLocal(Reference.MOD_ID+":gui.nei.arcaneWorkbench.shaped");
 	}
 
 	@Override
@@ -40,7 +38,8 @@ public class ShapedArcaneWorkbenchHandler extends TemplateRecipeHandler {
 	public void drawBackground(int recipe) {
 		GL11.glColor4f(1, 1, 1, 1);
 		GuiDraw.changeTexture(getGuiTexture());
-		GuiDraw.drawTexturedModalRect(0, 0, 5, 11, 166, 130);
+		GuiDraw.drawTexturedModalRect(0, 0, 11, 10, 123, 126);
+		GuiDraw.drawTexturedModalRect(126, 0, 147, 10, 75, 100);//FIXME: Add support to show wands
 	}
 
 	@Override
@@ -53,11 +52,31 @@ public class ShapedArcaneWorkbenchHandler extends TemplateRecipeHandler {
 		CachedShapedArcaneWorkbenchRecipe r = (CachedShapedArcaneWorkbenchRecipe) arecipes.get(recipe);
 		for (Aspect aspect : r.aspects.getAspects()){
 			if (aspect.isPrimal()) {
-				if (aspect.getName().equalsIgnoreCase("ignis")) {//Oh no, no switch statement! D:
-
-				}else if (aspect.getName().equalsIgnoreCase("ignis")) {
-
+				Color color = new Color(aspect.getColor());
+				GL11.glColor4f(color.getRed()/255f, color.getGreen()/255f, color.getBlue()/255f, 1f);
+				GuiDraw.changeTexture(aspect.getImage());
+				GL11.glScalef(.065f,.065f,.065f);
+				int coords[] = {0,0,0,0,260,260};
+				if (aspect.getName().equalsIgnoreCase("Ignis")) {//Oh no, no switch statement! D:
+					coords[0] = 78;
+					coords[1] = 1281;
+				}else if (aspect.getName().equalsIgnoreCase("Aer")) {
+					coords[0] = 808;
+					coords[1] = 48;
+				}else if (aspect.getName().equalsIgnoreCase("Terra")) {
+					coords[0] = 78;
+					coords[1] = 387;
+				}else if (aspect.getName().equalsIgnoreCase("Aqua")) {
+					coords[0] = 808;
+					coords[1] = 1621;
+				}else if (aspect.getName().equalsIgnoreCase("Ordo")) {
+					coords[0] = 1540;
+					coords[1] = 1281;
+				}else if (aspect.getName().equalsIgnoreCase("Perditio")) {
+					coords[0] = 1540;
+					coords[1] = 387;
 				}
+				GuiDraw.drawTexturedModalRect(coords[0], coords[1], coords[2], coords[3], coords[4], coords[5]);
 			}
 		}
 	}
@@ -78,11 +97,12 @@ public class ShapedArcaneWorkbenchHandler extends TemplateRecipeHandler {
 	}
 
 	public class CachedShapedArcaneWorkbenchRecipe extends CachedRecipe{
-		private final int[] outCoords = {150,25};
-		private final int[] inCoords = {20,60,90,40,70,100};//3 x coords, then 3 y coords
+		private final int[] outCoords = {139,54};
+		private final int[] inCoords = {29,53,77,56,63,85};//3 x coords, then 3 y coords
 
 		private PositionedStack output;
 		private List<PositionedStack> inputs = new ArrayList<PositionedStack>();
+		//private PositionedStack extra = new PositionedStack()TODO add wand extras
 
 		public AspectList aspects;
 
@@ -92,40 +112,67 @@ public class ShapedArcaneWorkbenchHandler extends TemplateRecipeHandler {
 			Object[] input = recipe.getInput();
 			int i = 0;
 			for (Object inputItem : input){
-				if (inputItem != null){
+				//if (inputItem != null){
 					if (inputItem instanceof ItemStack) {
 						switch (i) {
 							case 0:
-								this.inputs.add(new PositionedStack(inputItem, inCoords[0], inCoords[0]));
+								if (inputItem != null) {
+
+										this.inputs.add(new PositionedStack(inputItem, inCoords[0], inCoords[0]));
+								}
 								break;
 							case 1:
-								this.inputs.add(new PositionedStack(inputItem, inCoords[1], inCoords[0]));
+								if (inputItem != null){
+										this.inputs.add(new PositionedStack(inputItem, inCoords[1], inCoords[0]));
+								}
 								break;
 							case 2:
-								this.inputs.add(new PositionedStack(inputItem, inCoords[2], inCoords[0]));
+								if (inputItem != null){
+										this.inputs.add(new PositionedStack(inputItem, inCoords[2], inCoords[0]));
+
+								}
 								break;
 							case 3:
-								this.inputs.add(new PositionedStack(inputItem, inCoords[0], inCoords[1]));
+								if (inputItem != null){
+										this.inputs.add(new PositionedStack(inputItem, inCoords[0], inCoords[1]));
+
+								}
 								break;
 							case 4:
-								this.inputs.add(new PositionedStack(inputItem, inCoords[1], inCoords[1]));
+								if (inputItem != null){
+										this.inputs.add(new PositionedStack(inputItem, inCoords[1], inCoords[1]));
+
+								}
 								break;
 							case 5:
-								this.inputs.add(new PositionedStack(inputItem, inCoords[2], inCoords[1]));
+								if (inputItem != null){
+										this.inputs.add(new PositionedStack(inputItem, inCoords[2], inCoords[1]));
+
+								}
 								break;
 							case 6:
-								this.inputs.add(new PositionedStack(inputItem, inCoords[0], inCoords[2]));
+								if (inputItem != null){
+										this.inputs.add(new PositionedStack(inputItem, inCoords[0], inCoords[2]));
+
+								}
 								break;
 							case 7:
-								this.inputs.add(new PositionedStack(inputItem, inCoords[1], inCoords[2]));
+								if (inputItem != null){
+										this.inputs.add(new PositionedStack(inputItem, inCoords[1], inCoords[2]));
+
+								}
 								break;
 							case 8:
-								this.inputs.add(new PositionedStack(inputItem, inCoords[2], inCoords[2]));
+								if (inputItem != null){
+
+										this.inputs.add(new PositionedStack(inputItem, inCoords[2], inCoords[2]));
+
+								}
 								break;
 						}
 					}
 					i++;
-				}
+				//}
 			}
 		}
 
