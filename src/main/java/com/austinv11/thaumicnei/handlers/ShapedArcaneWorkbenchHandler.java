0fddf50ad55a14fceec01486757194afbcd3f8f6
austinv11 <austinv11@gmail.com>
2014-09-24 21:03:00 -0400

    Fixing aspect rendering

--- src/main/java/com/austinv11/thaumicnei/handlers/ShapedArcaneWorkbenchHandler.java
+++ src/main/java/com/austinv11/thaumicnei/handlers/ShapedArcaneWorkbenchHandler.java
@@ -50,46 +50,57 @@ public class ShapedArcaneWorkbenchHandler extends TemplateRecipeHandler {
 	@Override
 	public void drawExtras(int recipe) {
 		CachedShapedArcaneWorkbenchRecipe r = (CachedShapedArcaneWorkbenchRecipe) arecipes.get(recipe);
-		for (Aspect aspect : r.aspects.getAspects()){
+		int coords[] = {0,0,0,0,260,260};
+		int textCoords[] = {0,0};
+		for (Aspect aspect : r.aspects.getAspects()) {
 			if (aspect.isPrimal()) {
-				int coords[] = {0,0,0,0,260,260};
-				int textCoords[] = {0,0};
 				if (aspect.getName().equalsIgnoreCase("Ignis")) {//Oh no, no switch statement! D:
-					coords[0] = 78;
-					coords[1] = 1281;
 					textCoords[0] = 8;
 					textCoords[1] = 101;
-				}else if (aspect.getName().equalsIgnoreCase("Aer")) {
-					coords[0] = 808;
-					coords[1] = 48;
+				} else if (aspect.getName().equalsIgnoreCase("Aer")) {
 					textCoords[0] = 68;
 					textCoords[1] = 17;
-				}else if (aspect.getName().equalsIgnoreCase("Terra")) {
-					coords[0] = 78;
-					coords[1] = 387;
+				} else if (aspect.getName().equalsIgnoreCase("Terra")) {
 					textCoords[0] = 8;
 					textCoords[1] = 43;
-				}else if (aspect.getName().equalsIgnoreCase("Aqua")) {
-					coords[0] = 808;
-					coords[1] = 1621;
+				} else if (aspect.getName().equalsIgnoreCase("Aqua")) {
 					textCoords[0] = 68;
 					textCoords[1] = 98;
-				}else if (aspect.getName().equalsIgnoreCase("Ordo")) {
-					coords[0] = 1540;
-					coords[1] = 1281;
+				} else if (aspect.getName().equalsIgnoreCase("Ordo")) {
 					textCoords[0] = 104;
 					textCoords[1] = 101;
-				}else if (aspect.getName().equalsIgnoreCase("Perditio")) {
-					coords[0] = 1540;
-					coords[1] = 387;
+				} else if (aspect.getName().equalsIgnoreCase("Perditio")) {
 					textCoords[0] = 104;
 					textCoords[1] = 43;
 				}
-				GuiDraw.drawString(r.aspects.getAmount(aspect)+"", textCoords[0], textCoords[1], 0xFFFFFF, true);
+				GuiDraw.drawString(r.aspects.getAmount(aspect) + "", textCoords[0], textCoords[1], 0xFFFFFF, true);
+			}
+		}
+		GL11.glScalef(.065f,.065f,.065f);
+		for (Aspect aspect : r.aspects.getAspects()){
+			if (aspect.isPrimal()) {
+				if (aspect.getName().equalsIgnoreCase("Ignis")) {//Oh no, no switch statement! D:
+					coords[0] = 78;
+					coords[1] = 1281;
+				}else if (aspect.getName().equalsIgnoreCase("Aer")) {
+					coords[0] = 808;
+					coords[1] = 48;
+				}else if (aspect.getName().equalsIgnoreCase("Terra")) {
+					coords[0] = 78;
+					coords[1] = 387;
+				}else if (aspect.getName().equalsIgnoreCase("Aqua")) {
+					coords[0] = 808;
+					coords[1] = 1621;
+				}else if (aspect.getName().equalsIgnoreCase("Ordo")) {
+					coords[0] = 1540;
+					coords[1] = 1281;
+				}else if (aspect.getName().equalsIgnoreCase("Perditio")) {
+					coords[0] = 1540;
+					coords[1] = 387;
+				}
 				Color color = new Color(aspect.getColor());
 				GL11.glColor4f(color.getRed()/255f, color.getGreen()/255f, color.getBlue()/255f, 1f);
 				GuiDraw.changeTexture(aspect.getImage());
-				GL11.glScalef(.065f,.065f,.065f);
 				GuiDraw.drawTexturedModalRect(coords[0], coords[1], coords[2], coords[3], coords[4], coords[5]);
 			}
 		}
